05ba0c467d38e73fc6430e24a54e0805ce0e1c6d
ShuvrotoKumar <sdks6409@gmail.com>
2025-11-24 04:56:38 +0600

    completed the dashboard of copywright check web

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,112 @@
+import { createBrowserRouter } from "react-router-dom";
+import SignInPage from "../pages/auth/SignInPage";
+import ForgetPassword from "../pages/auth/ForgetPassword";
+import VerificationCode from "../pages/auth/VerificationCode";
+import ResetPassword from "../pages/auth/ResetPassword";
+import MainLayout from "../layout/MainLayout";
+import DashboardPage from "../pages/dashboard/DashboardPage";
+import PrivacyPolicy from "../pages/Privacy Policy/PrivacyPolicy";
+import TermsCondition from "../pages/Terms Condition/TermsCondition";
+import UserDetails from "../pages/userDetails/UserDetails";
+import CreateUser from "../pages/userDetails/CreateUser";
+import Notifications from "../pages/Notifications/Notifications";
+import ProfilePage from "../pages/profile/ProfilePage";
+import Reports from "../pages/Reports/Reports";
+import Settings from "../pages/Settings/Settings";
+import ChangePass from "../pages/profile/ChangePass";
+import AboutUs from "../pages/optional/AboutUs";
+import EditProfile from "../pages/profile/EditProfile";
+import CreateAdmin from "../pages/Create Admin/CreateAdmin";
+import AddAdmin from "../pages/Add Admin/AddAdmin";
+import PaymentManagement from "../pages/paymentManagement/PaymentManagement";
+
+const router = createBrowserRouter([
+  {
+    path: "/sign-in",
+    element: <SignInPage />,
+  },
+  {
+    path: "/forget-password",
+    element: <ForgetPassword />,
+  },
+  {
+    path: "/verification-code",
+    element: <VerificationCode />,
+  },
+  {
+    path: "/new-password",
+    element: <ResetPassword />,
+  },
+
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/user-details",
+        element: <UserDetails />,
+      },
+
+      {
+        path: "/payment-management",
+        element: <PaymentManagement />,
+      },
+      {
+        path: "/notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "/create-user",
+        element: <CreateUser />,
+      },
+
+      // settings
+      {
+        path: "/privacy-policy",
+        element: <PrivacyPolicy />,
+      },
+      {
+        path: "/terms-and-condition",
+        element: <TermsCondition />,
+      },
+      {
+        path: "/reports",
+        element: <Reports />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+      {
+        path: "/edit-profile",
+        element: <EditProfile />,
+      },
+      {
+        path: "/change-password",
+        element: <ChangePass />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUs />,
+      },
+      {
+        path: "/create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        path: "/add-admin",
+        element: <AddAdmin />,
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+    ],
+  },
+]);
+
+export default router;
